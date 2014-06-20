992145984c7c62d5cc0d022a301873c7721dbc21
unknown-user <unknown-user@fe80::adc2:7ff7:88e3:56%4>
2014-06-20 17:39:48 +0100

    Initial check-in of module Tetris

---
+++ TetrisModel/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿module TetrisModel.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("TetrisModel")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Andy")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
---
+++ TetrisModel/TetrisModel.fs
@@ -0,0 +1,69 @@
+﻿namespace TetrisModel
+
+[<AutoOpen>]
+module TetrisModule =
+
+  type Colour = Blue | Green | Yellow | Pink | Red
+
+  type ShapeName = 
+    | L
+    | Square
+    | Line
+    | S
+    | Z
+
+  type Rotate = Clockwise | AntiClockwise
+  type Direction = Left | Right | Down
+  type Rotation = North | East | South | West
+  type Point = { X : int; Y : int }
+
+  type Shape = { name : ShapeName; topLeft : Point; currentRotation : Rotation }
+
+  type GameBoard = { ActiveShape : Shape ; Shapes : Shape list }
+  type Game = { Score : int; Level : int;  }
+
+  let shapeColour = function
+    | L -> Blue
+    | Square -> Green
+    | Line -> Yellow
+    | S -> Pink
+    | Z -> Red
+
+  let ShapeMoved shape = function
+    | Left -> { shape with topLeft = { X = shape.topLeft.X - 1; Y = shape.topLeft.Y } }
+    | Right -> { shape with topLeft = { X = shape.topLeft.X + 1; Y = shape.topLeft.Y } }
+    | Down -> { shape with topLeft = { Y = shape.topLeft.Y - 1; X = shape.topLeft.Y } }
+
+  let ShapeRotated shape = function
+    | Clockwise -> 
+        match shape.currentRotation with
+        | North -> { shape with currentRotation = East }
+        | East -> { shape with currentRotation = South }
+        | South -> { shape with currentRotation = West }
+        | West -> { shape with currentRotation = North }
+    | AntiClockwise -> 
+        match shape.currentRotation with
+        | North -> { shape with currentRotation = West }
+        | East -> { shape with currentRotation = North }
+        | South -> { shape with currentRotation = East }
+        | West -> { shape with currentRotation = South }
+
+  let isPositionValid gameBoard shape =
+    shape.topLeft.X >= 0
+
+  let hitBottom gameBoard shape =
+    shape.topLeft.Y = 0
+
+  let withShapeAdded gameBoard shape =
+    { gameBoard with Shapes = shape :: gameBoard.Shapes }
+
+  let ActiveShapeMoved gameBoard direction =
+    let shiftedShape = gameBoard.ActiveShape |> ShapeMoved <| direction
+    if isPositionValid gameBoard shiftedShape then
+      { gameBoard with ActiveShape = shiftedShape }
+    else if hitBottom gameBoard shiftedShape then
+      withShapeAdded gameBoard shiftedShape
+    else
+      gameBoard
+
+
